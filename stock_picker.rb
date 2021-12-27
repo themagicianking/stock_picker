5e737643ab1c8b63ba59b06a3bef5e6e1dff6e80
Thane Chaves <tmoonder@gmail.com>
2021-12-26 19:22:06 -0500

    Project is functional.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,9 +1,22 @@
 def stock_picker(stock_prices)
 x = stock_prices.length - 1
+possible_combos = []
+profit_list = []
+best_combo = {}
   for i in 0..x
     range_start = i + 1
     max = stock_prices[range_start..x].max.to_i
     min = stock_prices[i].to_i
-    puts max - min
+    profit = max - min
+    profit_list.push(profit)
+    possible_combos.push({max: max, min: min, profit: profit})
   end
+  best_profit = profit_list.max
+  possible_combos.each do |hash|
+    if hash.dig(:profit) == best_profit
+     best_combo = hash
+    end
+  end
+  solution = [stock_prices.index(best_combo[:min]), stock_prices.index(best_combo[:max])]
+  solution
 end
